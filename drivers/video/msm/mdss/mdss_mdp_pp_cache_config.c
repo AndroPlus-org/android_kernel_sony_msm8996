9c5f4d932745e46ee8ffaa12d768f5105194895d
tbalden <illespal@gmail.com>
2016-07-16 14:17:25 -0700

    mdss mdp: kcal for mdss_mdp_v1_7
    Adding the kcal interface by @savoca and
    @faux123
    porting from m9 version by @flar2
    
    Using the MDP 1_7 userspace structs to inject
    configurations of kcal to the pp_cache configs.
    Overriding userspace copy with memcpy when
    copy_from_user fails (in case of kcal interface
    used).
    
    Adding a simple mdp ctl struct caching to
    the kcal driver to grab ctl struct for config
    calls.
    
    Could be done nicer with direct interfacing
    to userspace, leaves some space to make the kernel
    crash if a third party app sends faulty configs,
    but there's no security issue or degradation
    of functionality with this memcpy patch.
    
    @tbalden
    
    Signed-off-by: flar2 <asegaert@gmail.com>

--- drivers/video/msm/mdss/mdss_mdp_pp_cache_config.c
+++ drivers/video/msm/mdss/mdss_mdp_pp_cache_config.c
@@ -679,9 +679,14 @@ static int pp_pcc_cache_params_v1_7(struct mdp_pcc_cfg_data *config,
 #endif /* CONFIG_FB_MSM_MDSS_SPECIFIC_PANEL */
 		if (copy_from_user(&v17_usr_config, config->cfg_payload,
 				   sizeof(v17_usr_config))) {
+#ifdef CONFIG_FB_MSM_MDSS_KCAL_CTRL
+			memcpy(&v17_usr_config, config->cfg_payload, sizeof(v17_usr_config));
+			ret = 0;
+#else
 			pr_err("failed to copy v17 pcc\n");
 			ret = -EFAULT;
 			goto pcc_config_exit;
+#endif
 		}
 #ifdef CONFIG_FB_MSM_MDSS_SPECIFIC_PANEL
 		}
@@ -1187,9 +1192,15 @@ static int pp_pa_cache_params_v1_7(struct mdp_pa_v2_cfg_data *config,
 
 	if (copy_from_user(&pa_usr_config, config->cfg_payload,
 			   sizeof(pa_usr_config))) {
+#ifdef CONFIG_FB_MSM_MDSS_KCAL_CTRL
+		pr_err("%s Failed to copy v1_7 PA - copying in kernel \n",__func__);
+		memcpy(&pa_usr_config, config->cfg_payload, sizeof(pa_usr_config));
+		ret = 0;
+#else
 		pr_err("Failed to copy v1_7 PA\n");
 		ret = -EFAULT;
 		goto pa_config_exit;
+#endif
 	}
 
 	if ((config->flags & MDP_PP_OPS_DISABLE)) {
