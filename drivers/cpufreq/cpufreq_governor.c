c789f2de15d10b489736fd8ec0a78a7be41e307c
alucard24 <dmbaoh2@gmail.com>
2016-09-01 21:27:20 +0200

    alucard, darkness cpu govs: save and restore tuners settings!

--- drivers/cpufreq/cpufreq_governor.c
+++ drivers/cpufreq/cpufreq_governor.c
@@ -449,10 +449,12 @@ int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 		} else if (dbs_data->cdata->governor == GOV_ALUCARD) {
 			ac_ops->get_cpu_frequency_table(cpu);
 			ac_ops->get_cpu_frequency_table_minmax(policy, cpu);
+			ac_ops->get_cpu_cached_tuners(policy, cpu);
 			ac_dbs_info->up_rate = 1;
 			ac_dbs_info->down_rate = 1;
 		} else if (dbs_data->cdata->governor == GOV_DARKNESS) {
 			dk_ops->get_cpu_frequency_table(cpu);
+			dk_ops->get_cpu_cached_tuners(policy, cpu);
 		} else {
 			od_dbs_info->rate_mult = 1;
 			od_dbs_info->sample_type = OD_NORMAL_SAMPLE;
@@ -469,8 +471,13 @@ int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 		break;
 
 	case CPUFREQ_GOV_STOP:
-		if (dbs_data->cdata->governor == GOV_CONSERVATIVE)
+		if (dbs_data->cdata->governor == GOV_CONSERVATIVE) {
 			cs_dbs_info->enable = 0;
+		} else if (dbs_data->cdata->governor == GOV_ALUCARD) {
+			ac_ops->set_cpu_cached_tuners(policy, cpu);
+		} else if (dbs_data->cdata->governor == GOV_DARKNESS) {
+			dk_ops->set_cpu_cached_tuners(policy, cpu);
+		}
 
 		gov_cancel_work(dbs_data, policy);
 
