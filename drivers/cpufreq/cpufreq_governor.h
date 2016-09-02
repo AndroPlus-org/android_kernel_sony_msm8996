221608290a94ea5d9b14361d8b3e66fc4f866820
alucard24 <dmbaoh2@gmail.com>
2016-09-02 21:11:31 +0200

    alucard, darkness and nightmare cpu govs: Enhanced store and restore tuners settings when hotplugging cpus!

--- drivers/cpufreq/cpufreq_governor.h
+++ drivers/cpufreq/cpufreq_governor.h
@@ -258,6 +258,7 @@ struct common_dbs_data {
 struct dbs_data {
 	struct common_dbs_data *cdata;
 	unsigned int min_sampling_rate;
+	unsigned int cpu;
 	int usage_count;
 	void *tuners;
 
@@ -281,18 +282,10 @@ struct ac_ops {
 	void (*get_cpu_frequency_table)(int cpu);
 	void (*get_cpu_frequency_table_minmax)(struct cpufreq_policy *policy, 
 			int cpu);
-	void (*set_cpu_cached_tuners)(struct cpufreq_policy *policy, 
-			int cpu);
-	void (*get_cpu_cached_tuners)(struct cpufreq_policy *policy, 
-			int cpu);
 };
 
 struct dk_ops {
 	void (*get_cpu_frequency_table)(int cpu);
-	void (*set_cpu_cached_tuners)(struct cpufreq_policy *policy, 
-			int cpu);
-	void (*get_cpu_cached_tuners)(struct cpufreq_policy *policy, 
-			int cpu);
 };
 
 static inline int delay_for_sampling_rate(unsigned int sampling_rate)
