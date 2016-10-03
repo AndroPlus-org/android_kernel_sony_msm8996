7323e6392961b4c1dd344c4fa814221a0642b247
AndroPlus <hosigumayuugi@gmail.com>
2016-10-03 23:12:11 +0900

    Update .gitignore

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm64"
-#define UTS_VERSION "#1 SMP PREEMPT Mon Oct 3 22:38:40 JST 2016"
-#define LINUX_COMPILE_BY "androplus"
-#define LINUX_COMPILE_HOST "sonymobile.com"
-#define LINUX_COMPILER "gcc version 4.9.4 20160401 (optimized) (UBERTC-4.9.4) "
