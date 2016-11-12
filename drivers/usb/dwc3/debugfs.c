cd7281669e469491813873dc9b5f0dbfb6cebe9d
AndroPlus <hosigumayuugi@gmail.com>
2016-11-12 13:59:36 +0900

    Revert "USB: dwc3: debugfs: Add boundary check in dwc3_store_ep_num()"
    This reverts commit bafa462f4f9f18cb06f5f2a921856e0174976571.

--- drivers/usb/dwc3/debugfs.c
+++ drivers/usb/dwc3/debugfs.c
@@ -630,7 +630,7 @@ static ssize_t dwc3_store_ep_num(struct file *file, const char __user *ubuf,
 	struct seq_file		*s = file->private_data;
 	struct dwc3		*dwc = s->private;
 	char			kbuf[10];
-	unsigned int		num, dir, temp;
+	unsigned int		num, dir;
 	unsigned long		flags;
 
 	memset(kbuf, 0, 10);
@@ -641,16 +641,8 @@ static ssize_t dwc3_store_ep_num(struct file *file, const char __user *ubuf,
 	if (sscanf(kbuf, "%u %u", &num, &dir) != 2)
 		return -EINVAL;
 
-	if (dir != 0 && dir != 1)
-		return -EINVAL;
-
-	temp = (num << 1) + dir;
-	if (temp >= (dwc->num_in_eps + dwc->num_out_eps) ||
-					temp >= DWC3_ENDPOINTS_NUM)
-		return -EINVAL;
-
 	spin_lock_irqsave(&dwc->lock, flags);
-	ep_num = temp;
+	ep_num = (num << 1) + dir;
 	spin_unlock_irqrestore(&dwc->lock, flags);
 
 	return count;
