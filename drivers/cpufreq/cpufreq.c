b75ea41b9c455d7f8c552cd59c70bd4f45ec70a0
Lucas Lee <lucasleeeeeeeee@gmail.com>
2017-04-15 01:04:13 +0800

    Revert "39.2.A.0.417"
    This reverts commit 057b7b3fb2c93db17a587d2a494d9a58065d6053.

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1329,8 +1329,7 @@ static int __cpufreq_add_dev(struct device *dev, struct subsys_interface *sif)
 	}
 	up_write(&policy->rwsem);
 
-/*	kobject_uevent(&policy->kobj, KOBJ_ADD); */
-	kobject_uevent(&dev->kobj, KOBJ_CHANGE);
+	kobject_uevent(&policy->kobj, KOBJ_ADD);
 
 	up_read(&cpufreq_rwsem);
 
