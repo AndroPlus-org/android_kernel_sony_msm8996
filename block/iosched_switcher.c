8167f8f7e223e8850787f726f4d008dbce5a171c
Joe Maples <joe@frap129.org>
2017-05-06 18:42:21 -0500

    iosched_switcher: Use power efficient workqueues
    There's no reason that the work can't be scheduled for a different core than it was run from, so let's save more power
    
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- block/iosched_switcher.c
+++ block/iosched_switcher.c
@@ -78,8 +78,9 @@ static int fb_notifier_callback(struct notifier_block *nb,
 		 */
 		if (delayed_work_pending(&sleep_sched))
 			cancel_delayed_work_sync(&sleep_sched);
-		schedule_delayed_work(&restore_prev,
-				msecs_to_jiffies(RESTORE_DELAY_MS));
+		queue_delayed_work(system_power_efficient_wq,
+			&restore_prev,
+			msecs_to_jiffies(RESTORE_DELAY_MS));
 		break;
 	default:
 		/*
