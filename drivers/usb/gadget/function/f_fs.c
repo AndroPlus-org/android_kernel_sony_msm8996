0628c2dbad1262b4c2dd6beab8057883e00840ec
GUYVER91 <BlackDeath1370@gmail.com>
2017-05-30 19:08:31 +0430

    usb: gadget: f_fs: remove Error: returning -512 value spam from logs

--- drivers/usb/gadget/function/f_fs.c
+++ drivers/usb/gadget/function/f_fs.c
@@ -974,8 +974,9 @@ error_lock:
 	mutex_unlock(&epfile->mutex);
 error:
 	kfree(data);
-	if (ret < 0)
-		pr_err_ratelimited("Error: returning %zd value\n", ret);
+	if (ret < 0 && ret != -ERESTARTSYS)
+		pr_err_ratelimited("%s(): Error: returning %zd value\n",
+							__func__, ret);
 	return ret;
 }
 
